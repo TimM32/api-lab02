47d761756acbc62002faf1a2f58a52e2a16761ee
TimM32 <timmaupin@live.com>
2023-04-25 07:30:51 -0500

    create error message and map location?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from 'axios';
 
+
 class App extends React.Component {
     constructor(props) {
         super(props)
@@ -11,22 +12,44 @@ class App extends React.Component {
             errorMessage: '',
             locationLat: '',
             locationLon: '',
+            locationMap: '',
         };
     }
 }
 
+handleCitySubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+
+        let url = (`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_KEY_LOCATION}&q=${this.state.city}&format=json`);
+
+        let cityInfo = await axios.get(url);
+
+        let locationMap = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY_LOCATION}&center=${cityInfo.data[0].lat},${cityInfo.data[0].lon}&zoom=12`
 
 
-// let url 
-// let cityInfo
 
 
-let locationLat = cityInfo.data[0].lat;
-let locationLon = cityInfo.data[0].lon;
 
-this.setState({
-    cityName: cityInfo.data[0],
-    error: false,
-    locationLat: locationLat,
-    locationLon: locationLon
-});
+
+
+        let locationLat = cityInfo.data[0].lat;
+        let locationLon = cityInfo.data[0].lon;
+
+        this.setState({
+            cityName: cityInfo.data[0],
+            error: false,
+            locationLat: locationLat,
+            locationLon: locationLon,
+            locationMap: locationMap,
+        });
+    } catch (error) {
+        this.setState({
+            error: true,
+            errorMessage: `Error has occured: ${error.response.status}`,
+        });
+    }
+};
+
+
