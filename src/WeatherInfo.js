9e1535583a68d95ff5cb5b943a4a92911f571ff9
Tim Maupin <timmaupin@live.com>
2023-04-28 09:01:52 -0500

    finish weatherinfo setup

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,3 +1,21 @@
 import React from "react";
 import { Card } from 'react-bootstrap'
 import './App.css'
+
+class WeatheInfo extends React.Component {
+    render() {
+        return (
+            <>
+                <Card id='forcast-display'>
+                    <Card.body>
+                        <Card.Text>Date: {this.props.day.date}</Card.Text>
+                        <Card.Text>Forecast: {this.props.day.forecast}</Card.Text>
+                    </Card.body>
+                </Card>
+            </>
+        )
+    }
+}
+
+
+export default WeatheInfo;
