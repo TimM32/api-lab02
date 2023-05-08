0a3dfc61336c2e0b8ca472fe33ec91d8bf23ce4e
Tim Maupin <timmaupin@live.com>
2023-05-08 09:25:26 -0500

    work on getting api to render

--- src/LatLon.js
+++ src/LatLon.js
@@ -8,8 +8,8 @@ class LatLon extends React.Component {
         <Card className="location-cards" id="location">
           <Card.Body>
             <Card.Title>Enjoy {this.props.city}!</Card.Title>
-            <Card.Text>
-                Latitude: {this.props.lat},
+            <Card.Text className='card-text'>
+                Latitude: {this.props.lat}<br></br>
                 Longitude:{this.props.lon}      
             </Card.Text>
           </Card.Body>
